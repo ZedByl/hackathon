6965bec7027848601855e8fe0719734cba5633f6
nikita <nik.cet@mail.ru>
2021-08-20 21:30:30 +0300

    add bootstrap

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import './styles.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import { ContextMenu } from './menu'
+import { MessageModule } from './modules/message.module'
 
-
-
-
+const message = new MessageModule('message', 'Важное сообщение')
 const menu = new ContextMenu()
 ContextMenu.open(menu.menu)
 ContextMenu.close(menu.menu)
-ContextMenu.add(menu.menu, null)
+ContextMenu.add(menu.menu, message)
 
-
+console.log(message)
---
+++ src/modules/message.module.js
@@ -0,0 +1,11 @@
+import {Module} from '../core/module'
+
+export class MessageModule extends Module {
+  constructor(type, text) {
+    super(type, text);
+  }
+
+  static message() {
+
+  }
+}
