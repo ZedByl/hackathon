5b8fad68bcdf566360bd988c3fd22e6710cb853c
Grigoriy <grisha14423@gmail.com>
2021-08-22 09:02:18 +0300

    Implemented clicks module,test-button in index.html

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
-import './styles.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import { ContextMenu } from './menu'
-import { MessageModule } from './modules/message.module'
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { ContextMenu } from "./menu";
+import { MessageModule } from "./modules/message.module";
+import { ClicksModule } from "./modules/clicks.module";
 
-const message = new MessageModule('message', 'Важное сообщение')
-const menu = new ContextMenu()
-ContextMenu.open(menu.menu)
-ContextMenu.close(menu.menu)
-ContextMenu.add(menu.menu, message)
+const message = new MessageModule("message", "Важное сообщение");
+const menu = new ContextMenu();
+ContextMenu.open(menu.menu);
+ContextMenu.close(menu.menu);
+ContextMenu.add(menu.menu, message);
 
-console.log(message)
+console.log(message);
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,58 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
+import "bootstrap/dist/css/bootstrap.css";
 
 export class ClicksModule extends Module {
+  constructor(type, text) {
+    super(type, text);
+  }
 
+  trigger() {
+    let button = document.querySelector("button");
+
+    let counter = 0;
+
+    let getCount = function () {
+      counter++;
+    };
+
+    button.addEventListener("click", (event) => {
+      event.stopPropagation();
+
+      let helperElement = document.createElement("p");
+      helperElement.className = "text-center text-danger";
+      document.body.append(helperElement);
+      helperElement.innerText = "Секунда на подготовку!";
+
+      let timerElement = document.createElement("p");
+      timerElement.className = "text-center text-primary";
+      document.body.append(timerElement);
+
+      let result = document.createElement("h1");
+      result.className = "text-center text-success";
+      document.body.append(result);
+      let timer = 4;
+
+      let timerId = setInterval(() => {
+        if (timer === 1) {
+          timerElement.remove();
+          result.innerHTML = `Итого кликов: ${counter}<br> <h6 class="text-center text-muted">Нажмите, чтобы удалить</h6>`;
+          result.addEventListener("click", () => {
+            result.remove();
+          });
+          counter = 0;
+          document.removeEventListener("click", getCount);
+          clearInterval(timerId);
+        } else {
+          timer--;
+        }
+        helperElement.remove();
+        timerElement.innerText = `Кликайте! Осталось секунд: ${timer}`;
+      }, 1000);
+
+      document.addEventListener("click", getCount);
+    });
+  }
 }
+
+let click = new ClicksModule("click", "click");
+click.trigger();
